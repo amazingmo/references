86548b20b142cb5d14f112cf0e52c4033caba475
Amazingmo <36059812+amazingmo@users.noreply.github.com>
2018-05-06 22:48:35 +1000

    One more page

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use std::collections::HashMap;
 
+// Weird, I know.
+// STASH, a global reference to an int32,
+// is a reference to the constant 128.
+// The syntax feels a little odd.
+// Particularly the location of the "static mut"
+// relative to the rest of the type.
+static mut STASH: &i32 = &128;
+static VALUE: i32 = 256;
+
 type Table = HashMap<String, Vec<String>>;
 
 /// t has to be a &mut type, otherwise you can't change it
@@ -24,6 +33,39 @@ fn show(t : &Table)
             println!("    {}", w);
         }
     }
+}
+
+// A prop for a later demonstration
+fn factorial(n: usize) -> usize
+{
+    (1 .. n+1).fold(1, |a, b| a * b)
+}
+
+// The rules about lifetimes of references mean that
+// if you're going to assign to a static reference, you
+// need to explicitly name the lifetimes
+fn danger(p : &'static i32)
+{
+    unsafe
+    {
+        STASH = p;
+    }
+}
+
+// Rust assumes that if there is one argument and a return
+// value, then they have the same lifetime. You don't have
+// to write it out explicitly like this:
+fn smallest<'a>(v : &'a [i32]) -> &'a i32
+{
+    let mut tmp = &v[0];
+    for r in &v[1..]
+    {
+        if *r < *tmp
+        {
+            tmp = r;
+        }
+    }
+    tmp
 }
 
 fn main()
@@ -87,5 +129,44 @@ fn main()
         let rr : &&Point = &r;
         let rrr : &&&Point = &rr;
         assert_eq!(rrr.y, 729);
+    }
+
+    // Comparisons are also magic. They see the thing being compared, so long as they have the same type
+    {
+        let p1 : i32 = 729;
+        let r1 : &i32 = &p1;
+        let rr1 : &&i32 = &r1;
+        let rrr1 : &&&i32 = &rr1;
+        
+        let p2 : i32 = 730;
+        let r2 : &i32 = &p2;
+        let rr2 : &&i32 = &r2;
+        let rrr2 : &&&i32 = &rr2;
+        
+        assert!(rrr1 < rrr2);
+        
+        // Does it work for unequal levels of references?
+        
+        assert!(rr1 < rrr2);
+        // sure does!
+    }
+    
+    // The ampersand can be applied in all manner of syntactic constructs
+    {
+        let r = &factorial(6);
+        assert_eq!(r, &720);
+    }
+    
+    // But be aware of the specificity required of lifetimes
+    danger(&VALUE);
+    
+    // In the following, s gets the lifetime of parabola because of the
+    // way that smallest is defined, but the explicit definition of
+    // lifetimes is only meant to highlight the default behaviour.
+    // It will fail if s is defined out of the inner scope.
+    {
+        let parabola = [9, 4, 1, 0, 1, 4, 9];
+        let s = smallest(&parabola);
+        assert_eq!(*s, 0);
     }
 }
